351230df11f49facaf551af46d2c18265e6a45f5
HM <hieumdd@gmail.com>
2023-10-09 16:47:29 +0700

    misc: Update loggings

--- src/logging.service.ts
+++ src/logging.service.ts
@@ -2,6 +2,7 @@ import { createLogger, format, transports } from 'winston';
 const { combine, printf } = format;
 
 export const logger = createLogger({
+    level: 'debug',
     format: combine(printf(({ level, message }) => JSON.stringify({ severity: level, ...message }))),
     transports: [new transports.Console()],
 });
