058a38ce438d2f49b81450ec6d0976c1090f3993
HM <hieumdd@gmail.com>
2024-01-22 10:08:57 +0700

    fix: Fix ticket-updated timestamp

--- src/ticket-updated/ticket-updated.service.ts
+++ src/ticket-updated/ticket-updated.service.ts
@@ -7,7 +7,15 @@ export const getTicketUpdatedStream = () => {
     const sql = qb
         .withSchema('OP_CDP')
         .from('Moengage_TicketStage')
-        .select(['u_mb', 'ticket_id', 'created_at', 'updated_at', 'stage', 'ttcs', 'nhu_cau'])
+        .select([
+            'u_mb',
+            'ticket_id',
+            qb.raw('timestamp_sub(created_at, interval 7 hour) as created_at'),
+            qb.raw('timestamp_sub(updated_at, interval 7 hour) as updated_at'),
+            'stage',
+            'ttcs',
+            'nhu_cau',
+        ])
         .whereRaw(`extract(date from updated_at) = date_add(CURRENT_DATE(), interval -1 day)`);
 
     return createQueryStream(
