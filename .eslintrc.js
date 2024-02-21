1c1260f034d2932a58f9f2d722d7d81df4a6adee
HM <hieumdd@gmail.com>
2024-02-21 16:35:59 +0700

    misc: Update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: { '@typescript-eslint/no-unused-vars': 0, '@typescript-eslint/no-explicit-any': 0 },
+};
