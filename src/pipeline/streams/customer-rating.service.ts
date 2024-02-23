76453ff7e966436e32b166f7fdcfba7b1afbe48a
HM <hieumdd@gmail.com>
2024-02-23 13:50:29 +0700

    feat: Add complimentStar

--- src/pipeline/streams/customer-rating.service.ts
+++ src/pipeline/streams/customer-rating.service.ts
@@ -6,7 +6,7 @@ export const getCustomerRatingStream = createEventStream({
     qb: qb
         .withSchema('OP_CDP')
         .from('Moengage_CustomersRating')
-        .select(['u_mb', 'u_n', 'u_id', 'create_date', 'rating_giaohang', 'rating_SO'])
+        .select(['u_mb', 'u_n', 'u_id', 'create_date', 'rating_giaohang', 'rating_SO', 'compliment_star'])
         .whereRaw(`create_date = date_add(current_date("Asia/Ho_Chi_Minh"), interval -1 day)`),
     schema: Joi.object({
         u_mb: Joi.string(),
@@ -15,6 +15,7 @@ export const getCustomerRatingStream = createEventStream({
         create_date: timestamp,
         rating_giaohang: Joi.string(),
         rating_SO: Joi.string(),
+        compliment_star: Joi.boolean(),
     }),
     customerId: (row) => row.u_mb,
     actionsMeta: (row) => ({ action: 'CustomersRating', current_time: row.create_date }),
